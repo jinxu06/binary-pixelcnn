f06fd4a9c2b91406b1612b1f6f5328c6064e043e
Jin Xu <aaron.jin.xu@gmail.com>
2018-07-08 15:47:43 +0800

    sync

--- blocks/plots.py
+++ blocks/plots.py
@@ -4,7 +4,7 @@ from PIL import Image
 def tile_images(imgs, size=(6, 6)):
     imgs = imgs[:size[0]*size[1], :, :, :]
     if imgs.shape[-1] == 1:
-        imgs = np.stack([imgs.copy()[:,:,:,0] for k in range(3)])
+        imgs = np.stack([imgs.copy()[:,:,:,0] for k in range(3)], axis=-1)
     img_h, img_w = imgs.shape[1], imgs.shape[2]
     all_images = np.zeros((img_h*size[0], img_w*size[1], 3), np.uint8)
     for j in range(size[0]):
@@ -12,7 +12,7 @@ def tile_images(imgs, size=(6, 6)):
             all_images[img_h*j:img_h*(j+1), img_w*i:img_w*(i+1), :] = imgs[j*size[1]+i, :, :, :]
     return all_images
 
-def visualize_samples(images, name="results/test.png", layout=[5,5], vrange=[-1., 1.]):
+def visualize_samples(images, name="results/test.png", layout=[5,5], vrange=[0., 1.]):
     images = (images - vrange[0]) / (vrange[1]-vrange[0]) * 255.
     images = np.rint(images).astype(np.uint8)
     view = tile_images(images, size=layout)

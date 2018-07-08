2dfd1276de1c96937006813b7e5a4274c40bed45
Jin Xu <aaron.jin.xu@gmail.com>
2018-07-08 12:33:42 +0800

    sync

--- data/mnist.py
+++ data/mnist.py
@@ -4,8 +4,8 @@ from data.dataset import Dataset
 import blocks.helpers as helpers
 
 
-def load(data_dir, num_classes, batch_size, split=[5./7, 1./7, 1./7], one_hot=True, return_meta=False):
-    classes = [0, 1, 2, 3, 4] # np.random.choice(10, num_classes, replace=False).astype(np.int32)
+def load(data_dir, num_classes, batch_size, split=[5./7, 1./7, 1./7], one_hot=True):
+    classes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] # np.random.choice(10, num_classes, replace=False).astype(np.int32)
     from sklearn.datasets import fetch_mldata
     mnist = fetch_mldata('MNIST original', data_home=data_dir)
     images = mnist['data'].astype(np.float32)
@@ -16,6 +16,7 @@ def load(data_dir, num_classes, batch_size, split=[5./7, 1./7, 1./7], one_hot=Tr
             imgs.append(img)
             ts.append(t)
     images, targets = np.array(imgs), np.array(ts)
+    images /= images.max()
     p = np.random.permutation(images.shape[0])
     images, targets = images[p], targets[p]
     num_samples = images.shape[0]
@@ -23,19 +24,6 @@ def load(data_dir, num_classes, batch_size, split=[5./7, 1./7, 1./7], one_hot=Tr
     split[-1] += num_samples - np.sum(split)
     datasets = []
     begin = 0
-    if return_meta:
-        for s in split:
-            end = begin + s
-            X = images[begin:end]
-            X = np.reshape(X, newshape=(s, 28, 28))
-            y = targets[begin:end]
-            # y -= np.min(y)
-            # if one_hot:
-            #     y = helpers.one_hot(y, len(classes))
-            dataset = MNIST(inner_batch_size=batch_size, X=X, y=y)
-            datasets.append(dataset)
-            begin = end
-        return datasets
     for s in split:
         end = begin + s
         X = images[begin:end]

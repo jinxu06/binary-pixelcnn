c94fb6a8a7f18c5b7e684982a6104d6e300dffbd
Jin Xu <aaron.jin.xu@gmail.com>
2018-07-09 10:58:54 +0800

    sync

--- blocks/layers.py
+++ blocks/layers.py
@@ -16,13 +16,14 @@ def conv2d(inputs, num_filters, kernel_size, strides=1, padding='SAME', nonlinea
 @add_arg_scope
 def deconv2d(inputs, num_filters, kernel_size, strides=1, padding='SAME', nonlinearity=None, bn=True, kernel_initializer=None, kernel_regularizer=None, is_training=False):
     outputs = tf.layers.conv2d_transpose(inputs, num_filters, kernel_size=kernel_size, strides=strides, padding=padding, kernel_initializer=kernel_initializer, kernel_regularizer=kernel_regularizer)
+    outputs = tf.pad(outputs, [[0,0], [0,1], [0,1], [0,0]])
     print("*", padding, int_shape(inputs), int_shape(outputs))
     if bn:
         outputs = tf.layers.batch_normalization(outputs, training=is_training)
     if nonlinearity is not None:
         outputs = nonlinearity(outputs)
     print("    + deconv2d", int_shape(inputs), int_shape(outputs), nonlinearity, bn)
-    return outputs 
+    return outputs
 
 @add_arg_scope
 def dense(inputs, num_outputs, nonlinearity=None, bn=False, kernel_initializer=None, kernel_regularizer=None, is_training=False):

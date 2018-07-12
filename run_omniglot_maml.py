4fb177447b4f4cd23555152f4cc64f1a936d8328
Jin Xu <aaron.jin.xu@gmail.com>
2018-07-12 12:07:44 +0800

    sync

--- run_omniglot_maml.py
+++ run_omniglot_maml.py
@@ -97,12 +97,12 @@ with tf.Session(config=config) as sess:
     params = {
         "eval_num_tasks": 5,
         "meta_iter_per_epoch": 5,
-        "meta_batch_size": 4,
+        "meta_batch_size": 5,
         "meta_step_size": 0.1,
-        "num_shots": 12,
-        "test_shots": 8,
-        "inner_iter": 4,
-        "inner_batch_size": 4,
+        "num_shots": 10,
+        "test_shots": 10,
+        "inner_iter": 5,
+        "inner_batch_size": args.batch_size,
     }
 
     mlearner = MetaLearner(session=sess, parallel_models=models, optimize_op=train_step, train_set=meta_train_set, eval_set=meta_eval_set, variables=tf.trainable_variables())

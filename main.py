5cc4df89a23b84b04413d87d14c58644e814c0da
Jin Xu <aaron.jin.xu@gmail.com>
2018-07-21 11:27:05 +0800

    sync

--- main.py
+++ main.py
@@ -2,18 +2,40 @@
 import numpy as np
 from data.gpsampler import GPSampler
 
-gp = GPSampler()
-xs, ys = gp.sample(100)
 
+gpsampler = GPSampler(input_range=[-2., 2.], var_range=[0.5, 0.5], max_num_samples=200)
 
-import matplotlib.pyplot as plt
-plt.style.use("ggplot")
-plt_vals = []
-for i in range(0, 5):
-    xs, ys = gp.sample(100, [-2, 2], var_range=[0.5,2.])
-    p = np.argsort(xs)
-    xs = xs[p]
-    ys = ys[p]
-    plt_vals.extend([xs, ys, "x-"])
-plt.plot(*plt_vals)
-plt.show()
+gpfuncs = []
+for k in range(600):
+    print(k)
+    gpfuncs += gpsampler.sample(100)
+
+xs, ys = [], []
+for f in gpfuncs:
+    x, y = f.get_all_samples()
+    xs.append(x)
+    ys.append(y)
+xs = np.array(xs)
+ys = np.array(ys)
+
+print(xs.shape)
+print(ys.shape)
+
+np.savez("gpsamples_var05", xs=xs, ys=ys)
+data = np.load("gpsamples_var05.npz")
+xs, ys = data["xs"], data["ys"]
+
+print(xs.shape)
+print(ys.shape)
+# import matplotlib.pyplot as plt
+# plt.style.use("ggplot")
+# plt_vals = []
+# for f in gpfuncs:
+#     x, y = f.get_all_samples()
+#     x = x[:, 0]
+#     p = np.argsort(x)
+#     x = x[p]
+#     y = y[p]
+#     plt_vals.extend([x, y, "-"])
+# plt.plot(*plt_vals)
+# plt.show()

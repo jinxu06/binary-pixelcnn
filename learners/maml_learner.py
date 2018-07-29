af4e4f84abf4042b74bcb98b4a07f22900c93b0f
Jin Xu <aaron.jin.xu@gmail.com>
2018-07-29 21:37:36 +0800

    sync

--- learners/maml_learner.py
+++ learners/maml_learner.py
@@ -42,9 +42,10 @@ class MAMLLearner(Learner):
         feed_dict = {}
         for i, task in enumerate(tasks):
 
-            num_shots = np.random.randint(low=1, high=30)
-            test_shots = np.random.randint(low=1, high=10)
-            num_shots, test_shots = 20, 10
+            num_shots = np.random.randint(low=5, high=30)
+            test_shots = np.random.randint(low=5, high=30)
+            #num_shots, test_shots = 20, 10
+            test_shots = 50
 
             X_value, y_value = task.sample(num_shots+test_shots)
             X_c_value, X_t_value = X_value[:num_shots], X_value[num_shots:]
@@ -64,9 +65,10 @@ class MAMLLearner(Learner):
         ls = []
         for _ in range(eval_samples):
 
-            num_shots = np.random.randint(low=1, high=30)
-            test_shots = np.random.randint(low=1, high=10)
-            num_shots, test_shots = 20, 10
+            num_shots = np.random.randint(low=5, high=30)
+            test_shots = np.random.randint(low=5, high=30)
+            #num_shots, test_shots = 20, 10
+            test_shots = 50
 
             X_value, y_value = self.eval_set.sample(1)[0].sample(num_shots+test_shots)
             X_c_value, X_t_value = X_value[:num_shots], X_value[num_shots:]
